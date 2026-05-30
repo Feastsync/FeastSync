869f10ddb3d539be0b7a82fac0616e2666a2dc34
chinyereogugua <dorcaschinyere936@gmail.com>
2026-05-30 18:08:55 +0100

    i remove header component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Header from "./Components/Header.jsx"
 import Howitworks from "./Page/Howitworks.jsx"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Howitworks />} />
         <Route path="/howitworks" element={<Howitworks />} />
--- src/Page/Howitworks.jsx
+++ src/Page/Howitworks.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import Header from  "../Components/Header.jsx"
 import "./Css/Howitwork.css"
+import Heroimage from "../assets/BackgroundImage/heroimage.jpg"
 
 const Howitworks = () => {
   return (
     <main className='main-container'>
       <section className='hero-section'>
-        <img src={} alt="" />
+        <img src={Heroimage} alt="Hero" />
+        <div className='overlay'></div>
+        <div className='hero-content'>
+          <h1>
+            Secure. Transparent.
+            <br />
+            Seamless: How
+            <br />
+            FeastSync Works.
+          </h1>
+
+          <p>
+            Whether you are hosting an event or performing at one,
+            FeastSync is built to make the entire process transparent,
+            secure, and effortless — from first click to final payout.
+          </p>
+        </div>
       </section>
     </main>
   )
