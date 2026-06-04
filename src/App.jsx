739067d3d37cffc7d1c6a31afb8ef592e7a7932f
Maryjane Nmesoma <nmesomman@gmail.com>
2026-06-04 19:56:45 +0100

    cmpleted my forget password and reset password page

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ const App = () => {
         <Route path="getStarted" element={<GetStarted />}/>
         <Route path="about" element={<About />}/>
         <Route path="services" element={<Services />}/>
+        {/* <Route path="forgetPassword" element={<ForgetPassword />}/> */}
       </Routes>
     </BrowserRouter>
   );
