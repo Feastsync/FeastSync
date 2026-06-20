6ed3c543e71772739a6d8109750114b5b04b9016
Maryjane Nmesoma <nmesomman@gmail.com>
2026-06-20 21:39:03 +0100

    fix:comment the unknown import in the admin dashboard

--- src/Page/Admin/AdminDashboard.jsx
+++ src/Page/Admin/AdminDashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./admincss/AdminDashboard.css";
 import { Button } from 'antd';
-import AdminDashBoardLeft from './component/AdminDashBoardLeft';
-import AdminDashBoardHeader from './component/AdminDashBoardHeader';
-import AdmindashboardReview from './component/AdmindashboardReview';
-import AdminDashboardRevenue from './component/AdminDashboardRevenue';
-import AdminDashboardVerification from './component/AdminDashboardVerification';
+// import AdminDashBoardLeft from './component/AdminDashBoardLeft';
+// import AdminDashBoardHeader from './component/AdminDashBoardHeader';
+// import AdmindashboardReview from './component/AdmindashboardReview';
+// import AdminDashboardRevenue from './component/AdminDashboardRevenue';
+// import AdminDashboardVerification from './component/AdminDashboardVerification';
 
 import { FaArrowTrendUp } from "react-icons/fa6";
 
@@ -19,14 +19,13 @@ const AdminDashboard = () => {
            {/* <AdminDashBoardHeader /> */}
             <div>
           <div className='adminDashBoardDiv'>
-                <AdmindashboardReview />
-              <AdminDashboardRevenue />
-              <AdminDashboardVerification /></div>   
+                {/* <AdmindashboardReview /> */}
+              {/* <AdminDashboardRevenue /> */}
+              {/* <AdminDashboardVerification /> */}
+            </div>   
             </div>
         </div>
        </div>
- 
-  
     </div>
   )
 }
